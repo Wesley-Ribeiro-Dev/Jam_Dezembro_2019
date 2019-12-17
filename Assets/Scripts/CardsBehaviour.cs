7f1f52f102403c6807672f6611e29e678fca5f90
Wesley <wesleysilvaribeiro9@gmail.com>
2019-12-17 16:24:22 -0300

    Version 1.1

---
+++ Assets/Scripts/CardsBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsBehaviour : MonoBehaviour
+{
+    [SerializeField] private float delay;
+    public Animator animator;
+    private int cardsUp = 0;
+    public GameObject cardsCounter;
+    private bool canClick = false;
+
+    void Start()
+    {
+         StartCoroutine(TurnCardsDown());
+    }
+
+    void Update()
+    {
+        Debug.Log(cardsUp);
+         cardsUp = cardsCounter.GetComponent<CardsCounter>().counter;
+         if(cardsUp == 3)
+         {
+             animator.SetTrigger("Turn Down");
+         }
+    }
+
+    IEnumerator TurnCardsDown()
+    {
+            yield return new WaitForSeconds(delay);
+            animator.SetTrigger("Time to start");
+            canClick = true;
+    }
+
+    private void OnMouseDown() 
+    {
+        if((cardsUp < 3) && (canClick))
+        {
+            animator.SetTrigger("Turn Up");
+            cardsCounter.GetComponent<CardsCounter>().counter++;
+        }
+    }
+}
---
+++ Assets/Scripts/CardsCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsCounter : MonoBehaviour
+{
+    public int counter = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
